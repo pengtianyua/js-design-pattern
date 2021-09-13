d2b3a54ba6c71e556cb50e098ea8a47d8a6e8ac8
Pengtianyu <2501846063@qq.com>
2021-09-13 11:24:49 +0800

    发布订阅模式

---
+++ src/publishSubscribeMode.js
@@ -0,0 +1,33 @@
+/**
+ * 发布订阅模式
+ *
+ *  申请成功 触发对应的订单、消息、审核模块对应逻辑
+ *
+ * 各模块相互独立
+ * 存在一对多的依赖关系
+ * 依赖模块不稳定、依赖关系不稳定
+ * 各模块由不同的人员、团队开发
+ * */
+
+//  EventEmit
+const EventEmit = function () {
+	this.events = {}
+
+	//  订阅
+	this.on = function (name, cb) {
+		if (this.events[name]) {
+			this.events[name].push(cb)
+		} else {
+			this.events[name] = [cb]
+		}
+	}
+
+	//  发布
+	this.trigger = function (name, ...arg) {
+		if (this.events[name]) {
+			this.events[name].forEach(eventListener => {
+				eventListener(...arg)
+			})
+		}
+	}
+}
